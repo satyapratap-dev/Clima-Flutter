39db5694dbaa258cc23ed228bd34404b91291b20
FlutLab.io <>
2022-07-24 18:52:54 +0000

    Autosave

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -21,7 +21,8 @@ class _LocationScreenState extends State<LocationScreen> {
   }
 
   void updateUI() {
-    temparature = (widget.locationData['main']['temp']).cast<int>();
+    double temp = widget.locationData['main']['temp'];
+    temparature = temp.toInt();
     condition = widget.locationData['weather'][0]['id'];
     cityName = widget.locationData['name'];
   }
