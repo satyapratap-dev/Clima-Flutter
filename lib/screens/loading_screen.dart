e3916443d430206735bc0144a3a0f643b0b3f567
FlutLab.io <>
2022-07-24 03:40:14 +0000

    Autosave

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -23,19 +24,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return LocationScreen();
-                },
-              ),
-            );
-          },
-          child: Text('Get Location'),
+        // child: ElevatedButton(
+        //   onPressed: () {
+        //     //Get the current location
+        //     Navigator.push(
+        //       context,
+        //       MaterialPageRoute(
+        //         builder: (context) {
+        //           return LocationScreen();
+        //         },
+        //       ),
+        //     );
+        //   },
+        //   child: Text('Get Location'),
+        // ),
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 50.0,
         ),
       ),
     );
