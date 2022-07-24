e0482341abe946b07d4e7e45a13c139b9179a356
FlutLab.io <>
2022-07-24 06:34:18 +0000

    Autosave

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,11 @@
 import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:clima/services/location.dart';
+import 'package:geolocator/geolocator.dart';
+
+const String appId = "6e93b3d15872f914c6929fed9ea71e9a";
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,22 +16,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    var locationData = Location().getCurrentLocationData();
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) {
-          print(locationData);
-          return LocationScreen(locationData: locationData);
-        },
-      ),
-    );
+    Location().getCurrentLocationData();
   }
 
   @override
   void deactivate() {
     super.deactivate();
+  }
+
+  void getCurrentLocationData() async {
+    //Get the current location
+    try {
+      Position location = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$appId&units=metric');
+      var locationData = await networkHelper.getData();
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            print(locationData);
+            return LocationScreen(locationData: locationData);
+          },
+        ),
+      );
+    } catch (e) {
+      throw e;
+    }
   }
 
   @override
