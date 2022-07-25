511e6196c89da94e4edb938bb060bddec5e52a61
FlutLab.io <>
2022-07-25 21:52:46 +0000

    Autosave

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,7 +5,7 @@ const String appId = "6e93b3d15872f914c6929fed9ea71e9a";
 const String weatherURI = "https://api.openweathermap.org/data/2.5/weather";
 
 class WeatherModel {
-  Future<dynamic> getWeatherDatabyCityName(String cityName) async {
+  Future<dynamic> getWeatherDatabyCityName(dynamic cityName) async {
     NetworkHelper networkHelper = NetworkHelper('$weatherURI?lat=q=$cityName&appid=$appId&units=metric');
     var locationData = await networkHelper.getData();
     return locationData;
