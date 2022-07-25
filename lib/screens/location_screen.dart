0ddb69caa4d35fd031c7e2068f1443c830efc9f2
FlutLab.io <>
2022-07-25 21:53:45 +0000

    Autosave

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -75,8 +75,8 @@ class _LocationScreenState extends State<LocationScreen> {
                       );
 
                       if (cityName != null) {
-                        print(cityName);
-                        var weatherData = await weatherModel.getWeatherDatabyCityName(cityName.toString());
+                        var weatherData = await weatherModel.getWeatherDatabyCityName(cityName);
+                        print(weatherData);
                         updateUI(weatherData);
                       }
                     },
